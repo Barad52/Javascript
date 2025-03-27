0a1b049dc830876b95fffa4f392db9324b5e3954
Barad Piyush <159508818+Barad52@users.noreply.github.com>
2025-03-27 12:52:49 +0530

    Add files via upload

---
+++ OOPS/main.js
@@ -0,0 +1,29 @@
+class Car {
+    constructor(brand, model) {
+        this._brand = brand;
+        this._model = model;
+
+        // console.log(this._brand)
+        // console.log(this._model)
+
+    }
+    get brand() {
+        return this._brand;
+    }
+
+    set brand(newBrand) {
+        this._brand = newBrand;
+    }
+
+    displayInfo() {
+        console.log(`Car: ${this._brand}, Model: ${this._model}`);
+    }
+}
+
+// Creating an Object
+const car1 = new Car("Toyota", "Corolla");
+car1.displayInfo();
+
+car1.brand = "Honda";
+car1.brand = "suzuki"
+// console.log(car1.brand); 
